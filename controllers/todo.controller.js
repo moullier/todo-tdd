cdabbdc1817f2abce66f56ee34b537e7a71f2351
Maria Francis-Moullier <moullier@gmail.com>
2020-10-07 16:25:16 -0700

    debugging integration test

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -2,5 +2,5 @@ const TodoModel = require("../model/todo.model");
 
 exports.createTodo = (req, res, next) => {
     const createdModel = TodoModel.create(req.body);
-    res.status(201).json(createdModel);
+    return res.status(201).json(createdModel);
 };
